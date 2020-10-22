7f1f9fd9b09c588433dc26c51e579e0d39d9c584
zalabjir <71826608+zalabjir@users.noreply.github.com>
2020-10-22 11:28:53 +0200

    Add files via upload

---
+++ trideni.h
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+typedef std::vector<std::string>  seznam_slov
+
+void nacti(std::string nazev, seznam_slov & jmena);
+
+void serad(std::string jmena);
+
+int zeptej_se_jestli_vypsat();
+
+int vypis(std::string jmena);
